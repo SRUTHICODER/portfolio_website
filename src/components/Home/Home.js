0276703a3f17017bb55b61298cd9b53af709e347
Sruthi S <94464175+SRUTHICODER@users.noreply.github.com>
2024-12-23 21:35:26 +0530

    Add files via upload

---
+++ src/components/Home/Home.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import homeLogo from "../../Assets/home-main.svg"; // You can update this image if necessary
+import Particle from "../Particle";
+import Home2 from "./Home2";
+import Type from "./Type";
+
+function Home() {
+  return (
+    <section>
+      <Container fluid className="home-section" id="home">
+        <Particle />
+        <Container className="home-content">
+          <Row>
+            <Col md={7} className="home-header">
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+                Hi There!{" "}
+                <span className="wave" role="img" aria-labelledby="wave">
+                  👋🏻
+                </span>
+              </h1>
+
+              <h1 className="heading-name">
+                I'M
+                <strong className="main-name"> SRUTHI S</strong>
+              </h1>
+
+              <h3 className="heading-description">
+                A final year student at Kumaraguru College of Technology, Coimbatore.
+              </h3>
+
+              <div style={{ padding: 50, textAlign: "left" }}>
+                <Type />
+              </div>
+            </Col>
+
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <img
+                src={homeLogo}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: "450px" }}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+      <Home2 />
+    </section>
+  );
+}
+
+export default Home;
